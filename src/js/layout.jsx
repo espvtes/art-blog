76f3881cf664118ab05dfced92b6cb430e676fda
yusniel Espino <espvtes@gmail.com>
2019-07-03 16:23:28 -0400

    navbar, jumbotron

--- src/js/layout.jsx
+++ src/js/layout.jsx
@@ -7,7 +7,7 @@ import { Demo } from "./views/demo.jsx";
 import { Single } from "./views/single.jsx";
 import Store from "./store/appContext.jsx";
 
-import { Navbar } from "./component/navbar.jsx";
+// import { Navbar } from "./component/navbar.jsx";
 import { Footer } from "./component/footer.jsx";
 
 //create your first component
@@ -17,7 +17,7 @@ export class Layout extends React.Component {
 			<div className="d-flex flex-column h-100">
 				<BrowserRouter>
 					<ScrollToTop>
-						<Navbar />
+						{/* <Navbar /> */}
 						<Switch>
 							<Route exact path="/" component={Home} />
 							<Route path="/demo" component={Demo} />
--- src/js/views/home.jsx
+++ src/js/views/home.jsx
@@ -5,6 +5,112 @@ import "../../styles/home.css";
 
 export class Home extends React.Component {
 	render() {
-		return <div className="text-center mt-5">kello</div>;
+		return (
+			<div className="container-fluid">
+				<div className="row">
+					<div className="col-12 p-0">
+						<div className="jumbotron jumbotron-fluid" id="home">
+							<div className="container">
+								<h1 className="display-4">Fluid jumbotron</h1>
+								<p className="lead">
+									This is a modified jumbotron that occupies
+									the entire horizontal space of its parent.
+								</p>
+							</div>
+						</div>
+						<nav className="navbar sticky-top ">
+							<a className="navbar-brand" href="#">
+								<img
+									src="https://totaltrademanagement.files.wordpress.com/2012/05/paint-brush-psd50308.png"
+									className="nav-img"
+									width="50"
+									height="50"
+									alt=""
+								/>
+							</a>
+							<ul className="nav justify-content-end">
+								<li className="nav-item">
+									<a className="nav-link" href="#home">
+										Home
+									</a>
+								</li>
+								<li className="nav-item">
+									<a className="nav-link" href="#curriculum">
+										Curriculum
+									</a>
+								</li>
+								<li className="nav-item">
+									<a className="nav-link" href="#obras">
+										Obras
+									</a>
+								</li>
+								<li className="nav-item">
+									<a className="nav-link" href="#proyecto">
+										Proyectos
+									</a>
+								</li>
+								<li className="nav-item">
+									<a className="nav-link" href="#">
+										link
+									</a>
+								</li>
+								<li className="nav-item">
+									<a className="nav-link" href="#contacto">
+										Contacto
+									</a>
+								</li>
+							</ul>
+						</nav>
+						<div className="row px-4 py-4 autor">
+							<div className="col-12 col-sm-3 py-4 px-4">
+								<img
+									src="https://www.vincentvangogh.org/images/self-portrait.jpg"
+									className="img-autor img-fluid w-100"
+								/>
+							</div>
+							<div className="col-12 col-sm-9">
+								<h3 className="pt-4">
+									<em>Rydel Griñan Legra</em>
+								</h3>
+								<p className="text-justify">
+									Lorem Ipsum is simply dummy text of the
+									printing and typesetting industry. Lorem
+									Ipsum has been the industry is standard
+									dummy text ever since the 1500s, when an
+									unknown printer took a galley of type and
+									scrambled it to make a type specimen book.
+									It has survived not only f ive centuries,
+									but also the leap into electronic
+									typesetting, remaining essentially
+									unchanged. It was popularised in the 1960s
+									with the release of Letraset sheets
+									containing Lorem Ipsum passages, and more
+									recently with desktop publishing software
+									like Aldus PageMaker including versions of
+									Lorem Ipsum.
+								</p>
+								<p className="text-justify">
+									Lorem Ipsum is simply dummy text of the
+									printing and typesetting industry. Lorem
+									Ipsum has been the industry is standard
+									dummy text ever since the 1500s, when an
+									unknown printer took a galley of type and
+									scrambled it to make a type specimen book.
+									It has survived not only f ive centuries,
+									but also the leap into electronic
+									typesetting, remaining essentially
+									unchanged. It was popularised in the 1960s
+									with the release of Letraset sheets
+									containing Lorem Ipsum passages, and more
+									recently with desktop publishing software
+									like Aldus PageMaker including versions of
+									Lorem Ipsum.
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
 	}
 }
