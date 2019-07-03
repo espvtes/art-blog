0dc64fff4192ce9d7580c0cd134c52cacebd8290
yusniel Espino <espvtes@gmail.com>
2019-07-03 16:09:32 -0400

    first

--- src/js/views/home.jsx
+++ src/js/views/home.jsx
@@ -5,16 +5,6 @@ import "../../styles/home.css";
 
 export class Home extends React.Component {
 	render() {
-		return (
-			<div className="text-center mt-5">
-				<h1>Hello Rigo!</h1>
-				<p>
-					<img src={rigoImage} />
-				</p>
-				<a href="#" className="btn btn-success">
-					If you see this green button, bootstrap is working
-				</a>
-			</div>
-		);
+		return <div className="text-center mt-5">kello</div>;
 	}
 }
