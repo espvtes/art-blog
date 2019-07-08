77606a3b05261d4bb666703defbead766f98897e
yusniel Espino <espvtes@gmail.com>
2019-07-08 14:48:13 -0400

    navbar

--- src/js/component/curriculum.jsx
+++ src/js/component/curriculum.jsx
@@ -1,12 +1,81 @@
 import React from "react";
+
 import PropTypes from "prop-types";
 
 export class Curriculum extends React.Component {
 	render() {
 		return (
-			<div className="container-fluid">
-				<div className="row">
-					<div className="col-12">this is a curriculum</div>
+			<div className="container-fluid curriculum-body">
+				<div className="row pt-4 px-4">
+					<div className="col-12 py-4 px-4 shadow p-3 mb-5 rounded">
+						<h3>Curriculum</h3>
+						<ul>
+							<li>
+								This is a curriculum form with every data that
+								you passed in your life like an Art Plastic.
+							</li>
+							<li>
+								This is a curriculum form with every data that
+								you passed in your life like an Art Plastic.
+							</li>
+							<li>
+								This is a curriculum form with every data that
+								you passed in your life like an Art Plastic.
+							</li>
+							<li>
+								This is a curriculum form with every data that
+								you passed in your life like an Art Plastic.
+							</li>
+							<li>
+								This is a curriculum form with every data that
+								you passed in your life like an Art Plastic.
+							</li>
+							<li>
+								This is a curriculum form with every data that
+								you passed in your life like an Art Plastic.
+							</li>
+							<li>
+								This is a curriculum form with every data that
+								you passed in your life like an Art Plastic.
+							</li>
+							<li>
+								This is a curriculum form with every data that
+								you passed in your life like an Art Plastic.
+							</li>
+							<li>
+								This is a curriculum form with every data that
+								you passed in your life like an Art Plastic.
+							</li>
+							<li>
+								This is a curriculum form with every data that
+								you passed in your life like an Art Plastic.
+							</li>
+							<li>
+								This is a curriculum form with every data that
+								you passed in your life like an Art Plastic.
+							</li>
+							<li>
+								This is a curriculum form with every data that
+								you passed in your life like an Art Plastic.
+							</li>
+							<li>
+								This is a curriculum form with every data that
+								you passed in your life like an Art Plastic.
+							</li>
+							<li>
+								This is a curriculum form with every data that
+								you passed in your life like an Art Plastic.
+							</li>
+							<li>
+								This is a curriculum form with every data that
+								you passed in your life like an Art Plastic.
+							</li>
+							<li>
+								This is a curriculum form with every data that
+								you passed in your life like an Art Plastic.
+							</li>
+						</ul>
+					</div>
 				</div>
 			</div>
 		);
