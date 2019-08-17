3d7ae7946e82e59d04d709ece1b5bc48da10f91a
ESPINO <espvtes@gmail.com>
2019-08-17 23:46:57 +0000

    link

--- src/js/component/proyectos.jsx
+++ src/js/component/proyectos.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export class Proyectos extends React.Component {
 	render() {
 		return (
 			<div className="contianer-fluid bg-secondary">
-				<div className="container pt-4">
+				<div className="row pt-4 id" id="project-up">
+					<div className="col-12  pt-4" />
+				</div>
+				<div className="container pt-4 ">
 					<div className="row pt-4">
 						<div className="col-12 col-sm-4 px-4">
 							<img
@@ -67,6 +71,13 @@ export class Proyectos extends React.Component {
 							<div className="">this is the overlay</div>
 						</div>
 					</div>
+				</div>
+				<div className="up float-right pr-4 pb-4">
+					<button type="button" className="btn btn-warning">
+						<a className="" href="#project-up">
+							up
+						</a>
+					</button>
 				</div>
 			</div>
 		);
